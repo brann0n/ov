d5ba3acee68cfec2f1e32fe5087bf1242e2cd080
Brandon <brandon.abbenhuis@student.nhlstenden.com>
2021-07-03 00:35:47 +0200

    did an oopsie, now it works with the frontend again

--- api/bus.js
+++ api/bus.js
@@ -5,31 +5,6 @@ class BusHandler {
     constructor(expressApp, routePrefix) {
         this.app = expressApp;
         this.routePrefix = routePrefix;
-
-        this.testData = {
-            "QBUZZ_g300_1": {
-                "LineWheelchairAccessible": "ACCESSIBLE",
-                "TransportType": "BUS",
-                "DestinationName50": "Groningen",
-                "DataOwnerCode": "QBUZZ",
-                "DestinationCode": "329",
-                "LinePublicNumber": "300",
-                "LinePlanningNumber": "g300",
-                "LineName": "Emmen - Groningen",
-                "LineDirection": 1
-            },
-            "QBUZZ_g300_2": {
-                "LineWheelchairAccessible": "ACCESSIBLE",
-                "TransportType": "BUS",
-                "DestinationName50": "Emmen",
-                "DataOwnerCode": "QBUZZ",
-                "DestinationCode": "761",
-                "LinePublicNumber": "300",
-                "LinePlanningNumber": "g300",
-                "LineName": "Emmen - Groningen",
-                "LineDirection": 2
-            }
-        };
     }
 
     mapRoutes() {
@@ -45,20 +20,21 @@ class BusHandler {
         res.setHeader('Content-Type', 'application/json');
 
         //modify received data to be iterable:
-        let newArray = [];
-        for (const [key, value] of Object.entries(this.testData)) {
-            newArray.push({ 'key': key, 'value': value });
-        }
+
 
         this.__privateCheckLineStorage().then((json) => {
-            res.end(json);
-        });     
+            let newArray = [];
+            for (const [key, value] of Object.entries(json)) {
+                newArray.push({ 'key': key, 'value': value });
+            }
+            res.end(JSON.stringify(newArray));
+        });
     }
 
-    async __privateCheckLineStorage(){
+    async __privateCheckLineStorage() {
         let response = await fsp.readFile("./data/linedata.json", "utf8");
         console.log(response);
-        return response;
+        return JSON.parse(response);
     }
 }
 
